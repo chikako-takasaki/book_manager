c669544a374d63302d5fb0b94f5c426ee76ba749
Chikako Takasaki <a3x07.byd.chika@gmail.com>
2018-10-16 19:22:23 +0900

    fix super()

--- book_manager/forms.py
+++ book_manager/forms.py
@@ -20,7 +20,7 @@ class LoginForm(AuthenticationForm):
   """ログインフォーム"""
   
   def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+    super(LoginForm).__init__(*args, **kwargs)
     for field in self.fields.values():
       field.widget.attrs['class'] = 'form-control'
       field.widget.attrs['placeholder'] = field.label
